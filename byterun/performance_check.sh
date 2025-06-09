d7bd4366e7abfaac6aed0017af3fca580a1a25ce
ProgramSnail <purpurebacterium@yandex.ru>
2025-06-09 17:08:41 +0300

    use xmake for tests, xmake -O3

--- byterun/performance_check.sh
+++ byterun/performance_check.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-dune build > /dev/null
+xmake build
+cp "build/linux/x86_64/release/byterun" byterun.exe
+
+# dune build > /dev/null
 
 compiler=../_build/default/src/Driver.exe
 
--- byterun/xmake.lua
+++ byterun/xmake.lua
@@ -5,6 +5,7 @@ set_languages("c++20", "c11")
 
 target("byterun")
     set_kind("binary")
+    add_cxflags("-O3")
     add_includedirs("include", "../runtime")
     add_files("../runtime/**.c", "../runtime/**.S")
     add_files( "src/**.cpp", "src/**.c")
